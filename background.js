4dbf3f41818542cee77a0b87e35c35a418478e21
liathit <sssuvor66@gmail.com>
2014-05-21 23:37:18 +0400

    Create repo

---
+++ background.js
@@ -0,0 +1,74 @@
+var data = {
+    timeAlarm: 1,
+    alarmName: "ring",
+    notifUrl: "https://www.linux.org.ru/notifications-count",
+    checkMsgUrl: "https://www.linux.org.ru/notifications",
+    loginUrl: "https://www.linux.org.ru/login.jsp"
+
+}
+
+
+var getmsgcounter = function(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", data.notifUrl, true);
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 403) {
+                alert('Please authorize on linux.org.ru');
+		chrome.tabs.create({url: data.loginUrl});
+            }
+            if (xhr.status == 200) {
+                callback(xhr.responseText);
+            } else {
+                console.error("Error");
+            }
+        }
+    }
+
+}
+
+
+var updateicon = function() {
+    getmsgcounter(function(response) {
+	if (response > 0) {
+	    chrome.browserAction.setIcon({path: "images/unread_notification_icon.png"});
+	    chrome.browserAction.setBadgeText({text : response});
+	} else {
+	     chrome.browserAction.setBadgeText({text : ""});
+	     chrome.browserAction.setIcon({path:"images/default_icon.png"});
+	}
+    });
+}
+
+
+function opennewtab() {
+    chrome.tabs.create({
+        url: data.checkMsgUrl
+    });
+}
+
+chrome.browserAction.onClicked.addListener(opennewtab);
+
+chrome.alarms.create(data.alarmName, {
+    'periodInMinutes': data.timeAlarm
+});
+
+function alarmListener(alarm) {
+    if (alarm.name == data.alarmName) {
+        updateicon();
+        //console.log("Service load: " + alert.name);
+    } else {
+        console.error("Alarm err");
+    }
+}
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    switch (alarm.name) {
+        case data.alarmName:
+            alarmListener(alarm);
+            break;
+    }
+});
+
+window.onload = updateicon();
